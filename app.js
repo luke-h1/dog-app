93e9f7b7ee8139c7647d7923dcc38a9dd72b549d
luke-h1 <luke.howsam@yahoo.com>
2020-09-09 16:59:52 +0100

    update project async/await + user queries

--- app.js
+++ app.js
@@ -1,39 +1,63 @@
-const btn = document
-    .getElementById('getDogs')
-    .addEventListener('click', getImages);
-const results = document.getElementById('results');
-// API RESPONSE
-// MESSAGE: URL
-// STATUS: SUCCESS
+const dogOutput = document.getElementById('output');
+const query = document.getElementById('dog_breed');
+const form = document.getElementById('form-control');
 
-const API_URL = `https://dog.ceo/api/breeds/image/random`;
-
-async function getImages() {
-    try {
-        let start = new Date().getTime();
-        await fetch(API_URL)
-            .then((res) => res.json())
-            .then((res) => {
-                console.log(res);
-                const output = ` 
-                <div class="wrapper"> 
-                <div class="card mb-3">
-                <img class="card-img-top" src="${res.message}" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">Dogs ✅</h5>
-                  <p class="card-text"></p>
-                  <p class="card-text"><small class="text-muted"></small></p>
-                </div>
-                </div> 
-        `;
-                const results = document.querySelector('.results');
-                results.innerHTML = output;
-
-                let end = new Date().getTime();
-                let time = end - start;
-                console.log(`API Execution Time: ${time} Milliseconds`);
-            });
-    } catch (e) {
-        console.log(e);
-    }
+async function getDogData(e) {
+  e.preventDefault();
+  try {
+    let start = new Date().getTime();
+    const searchTerm = query.value;
+    const API_URL = `https://dog.ceo/api/breed/${searchTerm}/images/random`;
+    const res = await fetch(`${API_URL}`);
+    const json = await res.json();
+    let end = new Date().getTime();
+    let time = end - start;
+    console.log(`API Execution Time: ${time} Milliseconds`);
+    showDogData(json);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+function showDogData(json) {
+  console.log(json);
+  // message with json
+  // message with status
+}
+
+// EVENT LISTENERS
+form.addEventListener('submit', getDogData);
+
+// https://dog.ceo/api/breed/${query}/images/random
+
+// async function getImages() {
+//   try {
+//     let start = new Date().getTime();
+//     await fetch(API_URL)
+//       .then((res) => res.json())
+//       .then((res) => {
+//         console.log(res);
+//         const results = document.querySelector('.results');
+//         // console.log(res.message)
+
+//         // obj.forEach((item) => {
+//         //   results.innerHTML += `
+//         //                 <div class="wrapper">
+//         //             <div class="card mb-3">
+//         //             <img class="card-img-top" src="${res.message}" alt="Card image cap">
+//         //             <div class="card-body">
+//         //               <h5 class="card-title">Dogs ✅</h5>
+//         //               <p class="card-text"></p>
+//         //               <p class="card-text"><small class="text-muted"></small></p>
+//         //             </div>
+//         //             </div>
+//         //                 `;
+//         // });
+//       });
+//     let end = new Date().getTime();
+//     let time = end - start;
+//     console.log(`API Execution Time: ${time} Milliseconds`);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
